2a07c55fbdf521997087d7dc747744f53a09b1b6
Nikhil Pal <palnikhil9870@gmail.com>
2022-08-11 22:12:32 +0530

    some changes

---
+++ test.js
@@ -0,0 +1,7 @@
+const element = document.getElementById("popBtn");
+        element.addEventListener("click", myFunction);
+        
+        function myFunction() {
+          //document.getElementById("demo").innerHTML = "Hello World123";
+          console.log('Hi');
+        }
