e71102efd3857d896f3b3dd6a6f6b37ff4dc82f7
Nikhil Pal <palnikhil9870@gmail.com>
2022-08-11 17:30:06 +0530

    Update brainbehind.js

--- brainbehind.js
+++ brainbehind.js
@@ -1,2 +1,11 @@
 let fetchBtn = document.getElementById('fetchBtn');
 fetchBtn.addEventListener('click', buttonClickHandler);
+
+function buttonClickHandler(){
+    console.log('You have clicked the button.');
+
+
+    const  xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'nikhil.txt', true);
+}
