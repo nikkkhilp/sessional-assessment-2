3c458a128c49c5fb231f706d7b765c918882a2ae
Nikhil Pal <palnikhil9870@gmail.com>
2022-08-11 20:35:49 +0530

    efvceqv

--- brainbehind.js
+++ brainbehind.js
@@ -1,36 +1,7 @@
-let popBtn = document.getElementById('popBtn');
+const popBtn = document.getElementById('popBtn');
 popBtn.addEventListener('click', popHandler);
 
 function popHandler() {
-    console.log('You have clicked the pop handler');
-
-    // Instantiate an xhr object
-    const xhr = new XMLHttpRequest();
-
-    // Open the object
-    xhr.open('GET', 'http://dummy.restapiexample.com/api/v1/employees', true);
-
-
-    // What to do when response is ready
-    xhr.onload = function () {
-        if(this.status === 200){
-            let obj = JSON.parse(this.responseText);
-            console.log(obj);
-            let list = document.getElementById('list');
-            str = "";
-            for (key in obj)
-            {
-                str += `<li>${obj[key].employee_name} </li>`;
-            }
-            list.innerHTML = str;
-        }
-        else{
-            console.log("Some error occured")
-        }
-    }
-
-    // send the request
-    xhr.send();
-    console.log("We are done fetching employees!");
+    alert('Called');
     
 }
