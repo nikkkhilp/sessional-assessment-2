bdec9e49fa50f07a107febe9edb68439c0144678
Nikhil Pal <palnikhil9870@gmail.com>
2022-08-11 23:20:03 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const element = document.getElementById("popBtn");
-        element.addEventListener("click", myFunction);
-        
-        function myFunction() {
-          //document.getElementById("demo").innerHTML = "Hello World123";
-          console.log('Hi');
-        }
